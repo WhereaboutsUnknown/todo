6ce72a86d3abe18b4918bf290f81aa6fb061a3a9
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-11-14 16:47:14 +0500

    feat: add task search methods to repository

--- src/main/java/com/sagansar/todo/repository/TodoTaskRepository.java
+++ src/main/java/com/sagansar/todo/repository/TodoTaskRepository.java
@@ -2,8 +2,18 @@ package com.sagansar.todo.repository;
 
 import com.sagansar.todo.model.work.TodoTask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TodoTaskRepository extends JpaRepository<TodoTask, Long> {
+
+    List<TodoTask> findAllByWorkerId(Integer workerId);
+
+    List<TodoTask> findAllByManagerId(Integer managerId);
+
+    @Query("SELECT task FROM todo_task task JOIN task.status WHERE (task.status.statusName = 'TODO' OR task.status.statusName = 'DISCUSSION')")
+    List<TodoTask> findAllAvailable();
 }
