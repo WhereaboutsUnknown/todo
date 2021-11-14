775421bf3102b5e49f61aa9ff896876a8fdaaafb
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-11-15 00:02:19 +0500

    feat: add worker profile endpoint and task claim logic for worker

--- src/main/java/com/sagansar/todo/controller/WorkerController.java
+++ src/main/java/com/sagansar/todo/controller/WorkerController.java
@@ -1,21 +1,22 @@
 package com.sagansar.todo.controller;
 
-import com.sagansar.todo.controller.dto.PersonNameDto;
 import com.sagansar.todo.controller.dto.TaskShortDto;
+import com.sagansar.todo.controller.dto.WorkerDto;
 import com.sagansar.todo.controller.mapper.PersonMapper;
 import com.sagansar.todo.controller.mapper.TaskMapper;
+import com.sagansar.todo.controller.mapper.WorkerMapper;
+import com.sagansar.todo.infrastructure.exceptions.BadRequestException;
 import com.sagansar.todo.model.general.RoleEnum;
 import com.sagansar.todo.model.general.User;
-import com.sagansar.todo.model.manager.Manager;
 import com.sagansar.todo.model.worker.Worker;
 import com.sagansar.todo.repository.TodoTaskRepository;
 import com.sagansar.todo.repository.WorkerRepository;
+import com.sagansar.todo.service.DialogService;
+import com.sagansar.todo.service.TodoService;
 import com.sagansar.todo.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -33,6 +34,30 @@ public class WorkerController {
 
     @Autowired
     UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    DialogService dialogService;
+
+    @Autowired
+    TodoService todoService;
+
+    @Autowired
+
+    @GetMapping("/")
+    public WorkerDto getUserWorkerProfile() {
+        User currentUser = userDetailsService.getCurrentUser();
+        if (currentUser == null) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Пользователь не найден");
+        }
+        Worker worker = workerRepository.findByUserId(currentUser.getId());
+        if (worker == null) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Не найден профиль исполнителя");
+        }
+        if (!worker.isActive()) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Профиль исполнителя был заблокирован");
+        }
+        return WorkerMapper.workerToDto(worker);
+    }
 
     @GetMapping("/{workerId}/tasks")
     public List<TaskShortDto> getTakenTasks(@PathVariable(name = "workerId") Integer workerId) {
@@ -60,7 +85,19 @@ public class WorkerController {
                 .collect(Collectors.toList());
     }
 
-    private void checkWorkerRights(Integer workerId) {
+    @PostMapping("/{workerId}/todo/{taskId}")
+    public String claimTask(@PathVariable(name = "workerId") Integer workerId,
+                          @PathVariable(name = "taskId") Long taskId,
+                          @RequestParam(name = "message") String message) {
+        Worker worker = checkWorkerRights(workerId);
+        try {
+            return todoService.claimTask(worker, taskId, message);
+        } catch (BadRequestException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getResponseMessage());
+        }
+    }
+
+    private Worker checkWorkerRights(Integer workerId) {
         if (!userDetailsService.checkUserRights(RoleEnum.FREELANCER)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Доступ запрещен");
         }
@@ -73,5 +110,6 @@ public class WorkerController {
         if (!worker.isActive()) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Профиль исполнителя был заблокирован");
         }
+        return worker;
     }
 }
