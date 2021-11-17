c5c67585cad81dfbeab3d57eb88a697a4dfd53c6
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-11-17 23:07:50 +0500

    feat: add task request script

--- src/main/webapp/static/js/worker.profile.js
+++ src/main/webapp/static/js/worker.profile.js
@@ -1,14 +1,17 @@
+let workerId;
+let tasksCache = [];
+
 window.addEventListener("DOMContentLoaded", () => {
-    //получить const userid = ########## (после прохождения окна логина)
     function req() {
         const xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8080/worker") //заменить на url-сервера/file.json?id=userid
+        xhr.open("GET", "http://localhost:8080/worker")
         xhr.setRequestHeader("Content-type", "application/json; charset = utf-8");
         xhr.send();
         xhr.addEventListener("load", function () {
             if (xhr.status === 200) {
                 let data = JSON.parse(xhr.response);
                 console.log(data);
+                workerId = data.id;
 //                $(".profilePhoto").attr("src", `${data[0].photo}`);
                 $("#profile-name").append(`${data.name}`);
                 $("#profile-age").append(`${data.age}`);
@@ -18,5 +21,48 @@ window.addEventListener("DOMContentLoaded", () => {
             }
         })
     }
+    function tasks() {
+        const xhrtask = new XMLHttpRequest();
+        xhrtask.open("GET", "http://localhost:8080/worker/" + 1 + "/tasks")
+        xhrtask.setRequestHeader("Content-type", "application/json; charset = utf-8");
+        xhrtask.send();
+        xhrtask.addEventListener("load", function () {
+            if (xhrtask.status === 200) {
+                let data = JSON.parse(xhrtask.response);
+                console.log(data);
+                for (var i = 0; i < data.length; i++) {
+                    tasksCache.push(data[i]);
+                    $("#task-block-list").append(
+                        `
+                        <li>
+                            <div class="task-block" id="${data[i].id}">
+                                <h3 class="task-status" id="task-status">${data[i].status}</h3>
+                                <div class="task-block-info">
+                                    <div class="task-block-header">
+                                        <h3 class="task-header" id="task-header">${data[i].header}</h3>
+                                    </div>
+                                    <div class="task-block-stack">
+                                        <p class="task-stack" id="task-stack">${data[i].stack}</p>
+                                    </div>
+                                    <div class="task-block-bottom-container">
+                                        <div class="task-block-bottom-block task-block-person">
+                                            <a href="https://sagansar.com" title="${data[i].person.name}" class="task-person" id="task-person">
+                                                <img src="/static/images/person-icon.png" alt="${data[i].person.name}">
+                                            </a>
+                                        </div>
+                                        <div class="task-block-bottom-block task-block-deadline">
+                                            <p class="task-deadline" id="task-deadline">${data[i].deadline.replace('T', ' ')}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        `
+                    );
+                }
+            }
+        })
+    }
     req();
+    tasks()
 });
