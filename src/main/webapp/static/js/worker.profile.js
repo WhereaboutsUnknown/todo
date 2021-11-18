6c319628e32e4982f606d9ee8dbfa05af88d6f69
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-11-18 09:45:05 +0500

    feat: script for refresh button

--- src/main/webapp/static/js/worker.profile.js
+++ src/main/webapp/static/js/worker.profile.js
@@ -1,39 +1,19 @@
 let workerId;
 let tasksCache = [];
 
-window.addEventListener("DOMContentLoaded", () => {
-    function req() {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://localhost:8080/worker")
-        xhr.setRequestHeader("Content-type", "application/json; charset = utf-8");
-        xhr.send();
-        xhr.addEventListener("load", function () {
-            if (xhr.status === 200) {
-                let data = JSON.parse(xhr.response);
-                console.log(data);
-                workerId = data.id;
-//                $(".profilePhoto").attr("src", `${data[0].photo}`);
-                $("#profile-name").append(`${data.name}`);
-                $("#profile-age").append(`${data.age}`);
-                $("#work-skills-info").append(`${data.skills}`);
-                $("#contacts-email").append(`${data.contacts.email}`);
-                $("#contacts-telegram").append(`${data.contacts.telegram}`);
-            }
-        })
-    }
-    function tasks() {
-        const xhrtask = new XMLHttpRequest();
-        xhrtask.open("GET", "http://localhost:8080/worker/" + 1 + "/tasks")
-        xhrtask.setRequestHeader("Content-type", "application/json; charset = utf-8");
-        xhrtask.send();
-        xhrtask.addEventListener("load", function () {
-            if (xhrtask.status === 200) {
-                let data = JSON.parse(xhrtask.response);
-                console.log(data);
-                for (var i = 0; i < data.length; i++) {
-                    tasksCache.push(data[i]);
-                    $("#task-block-list").append(
-                        `
+function tasks() {
+    const xhrtask = new XMLHttpRequest();
+    xhrtask.open("GET", "http://localhost:8080/worker/" + 1 + "/tasks")
+    xhrtask.setRequestHeader("Content-type", "application/json; charset = utf-8");
+    xhrtask.send();
+    xhrtask.addEventListener("load", function () {
+        if (xhrtask.status === 200) {
+            let data = JSON.parse(xhrtask.response);
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                tasksCache.push(data[i]);
+                $("#task-block-list").append(
+                    `
                         <li>
                             <div class="task-block" id="${data[i].id}">
                                 <h3 class="task-status" id="task-status">${data[i].status}</h3>
@@ -58,11 +38,37 @@ window.addEventListener("DOMContentLoaded", () => {
                             </div>
                         </li>
                         `
-                    );
-                }
+                );
+            }
+        }
+    })
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    function req() {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://localhost:8080/worker")
+        xhr.setRequestHeader("Content-type", "application/json; charset = utf-8");
+        xhr.send();
+        xhr.addEventListener("load", function () {
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.response);
+                console.log(data);
+                workerId = data.id;
+//                $(".profilePhoto").attr("src", `${data[0].photo}`);
+                $("#profile-name").append(`${data.name}`);
+                $("#profile-age").append(`${data.age}`);
+                $("#work-skills-info").append(`${data.skills}`);
+                $("#contacts-email").append(`${data.contacts.email}`);
+                $("#contacts-telegram").append(`${data.contacts.telegram}`);
             }
         })
     }
     req();
-    tasks()
+    tasks();
+});
+
+$("#refresh-button").click(function () {
+    $("#task-block-list").empty();
+    tasks();
 });
