aed80739d58977b59d2809744947a26292edab96
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-18 20:46:22 +0500

    feat: add methods to publish task (Manager) fix: add validation for claiming tasks (task should be visible to all)

--- src/main/java/com/sagansar/todo/service/TodoService.java
+++ src/main/java/com/sagansar/todo/service/TodoService.java
@@ -6,8 +6,10 @@ import com.sagansar.todo.model.manager.Manager;
 import com.sagansar.todo.model.work.TodoStatus;
 import com.sagansar.todo.model.work.TodoTask;
 import com.sagansar.todo.model.worker.Worker;
+import com.sagansar.todo.model.worker.WorkerResponse;
 import com.sagansar.todo.repository.TodoStatusRepository;
 import com.sagansar.todo.repository.TodoTaskRepository;
+import com.sagansar.todo.repository.WorkerResponseRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
@@ -25,14 +27,18 @@ public class TodoService {
 
     private final NotificationService notificationService;
 
+    private final WorkerResponseRepository responseRepository;
+
     public TodoService(TodoTaskRepository todoTaskRepository,
                        TodoStatusRepository todoStatusRepository,
                        NotificationService notificationService,
-                       DialogService dialogService) {
+                       DialogService dialogService,
+                       WorkerResponseRepository responseRepository) {
         this.todoStatusRepository = todoStatusRepository;
         this.todoTaskRepository = todoTaskRepository;
         this.notificationService = notificationService;
         this.dialogService = dialogService;
+        this.responseRepository = responseRepository;
     }
 
     /**
@@ -47,6 +53,24 @@ public class TodoService {
     }
 
     /**
+     * Publish created task
+     *
+     * @param manager manager of current task
+     * @param taskId task ID
+     * @param visibleToAll false if only invited Workers see this task, true if everyone does
+     * @return published task
+     * @throws BadRequestException in case of invalid task ID
+     */
+    public TodoTask publishTask(@NonNull Manager manager, @NonNull Long taskId, boolean visibleToAll) throws BadRequestException {
+        TodoTask task = getValidTaskForPublishing(taskId);
+        checkManagerRightsOnTask(manager, task);
+        task.setStatus(getStatus(TodoStatus.Status.TODO));
+        task.setVisibleToAll(visibleToAll);
+
+        return todoTaskRepository.save(task);
+    }
+
+    /**
      * Perform claim step on task (from worker searching to discussion)
      *
      * @param worker claiming worker
@@ -55,8 +79,12 @@ public class TodoService {
      * @return success message
      * @throws BadRequestException in case of invalid task ID
      */
-    public String claimTask(@NonNull Worker worker, @NonNull Long taskId, String message) throws BadRequestException {
+    public TodoTask claimTask(@NonNull Worker worker, @NonNull Long taskId, String message) throws BadRequestException {
         TodoTask task = getValidTaskForClaim(taskId);
+        if (!task.isVisibleToAll()) {
+            throw new BadRequestException("Стать исполнителем для этой задачи можно только по приглашению!");
+        }
+        sendWorkerResponse(task, worker, message);
         dialogService.createDialog(task, worker.getUser(), message);
 
         if (!TodoStatus.Status.DISCUSSION.equals(task.getStatus().status())) {
@@ -66,7 +94,7 @@ public class TodoService {
         }
         notificationService.sendTaskClaimNotification(task.getManager().getUser(), task.getHeader(), worker.getName());
 
-        return "Отклик успешно отправлен!";
+        return task;
     }
 
     /**
@@ -77,15 +105,43 @@ public class TodoService {
      * @throws BadRequestException in case of invalid task ID
      */
     private TodoTask getValidTaskForClaim(@NonNull Long taskId) throws BadRequestException {
+        TodoTask task = getValidTask(taskId);
+        TodoStatus.Status statusEnum = task.getStatus().status();
+        if (!TodoStatus.Status.DISCUSSION.equals(statusEnum) && !TodoStatus.Status.TODO.equals(statusEnum)) {
+            throw new BadRequestException("Задача [" + taskId + "] имеет некорректный статус: " + statusEnum);
+        }
+        return task;
+    }
+
+    /**
+     * Validate TodoTask for publishing step and get valid one
+     *
+     * @param taskId task ID
+     * @return valid TodoTask
+     * @throws BadRequestException in case of invalid task ID
+     */
+    private TodoTask getValidTaskForPublishing(@NonNull Long taskId) throws BadRequestException {
+        TodoTask task = getValidTask(taskId);
+        TodoStatus.Status statusEnum = task.getStatus().status();
+        if (TodoStatus.Status.DRAFT.equals(statusEnum)) {
+            throw new BadRequestException("Задача уже была опубликована!");
+        }
+        return task;
+    }
+
+    /**
+     * Validate TodoTask for existence and having status and get valid one
+     *
+     * @param taskId task ID
+     * @return existing TodoTask with status
+     * @throws BadRequestException in case of no such task or task having no status
+     */
+    private TodoTask getValidTask(@NonNull Long taskId) throws BadRequestException {
         TodoTask task = todoTaskRepository.findById(taskId)
                 .orElseThrow(() -> new BadRequestException("Задача " + taskId + " не найдена"));
         TodoStatus status = task.getStatus();
         if (status == null) {
             throw new BadRequestException("У задачи [" + taskId + "] отсутствует статус!");
-        }
-        TodoStatus.Status statusEnum = status.status();
-        if (!TodoStatus.Status.DISCUSSION.equals(statusEnum) && !TodoStatus.Status.TODO.equals(statusEnum)) {
-            throw new BadRequestException("Задача [" + taskId + "] имеет некорректный статус: " + statusEnum);
         }
         return task;
     }
@@ -120,5 +176,21 @@ public class TodoService {
         }
         return todoStatusRepository.findById(status.getCode())
                 .orElseThrow(() -> new RuntimeException("Нарушение данных: не найден статус " + status));
+    }
+
+    private void checkManagerRightsOnTask(Manager manager, TodoTask task) throws BadRequestException {
+        Manager taskManager = task.getManager();
+        if (taskManager == null || !manager.getId().equals(taskManager.getId())) {
+            throw new BadRequestException("Невозможно опубликовать: за задачу отвечает другой менеджер");
+        }
+    }
+
+    private void sendWorkerResponse(TodoTask task, Worker worker, String message) {
+        WorkerResponse response = new WorkerResponse();
+        response.setWorker(worker);
+        response.setTask(task);
+        response.setCreationTime(LocalDateTime.now(ZoneId.systemDefault()));
+        response.setMessage(message);
+        responseRepository.save(response);
     }
 }
