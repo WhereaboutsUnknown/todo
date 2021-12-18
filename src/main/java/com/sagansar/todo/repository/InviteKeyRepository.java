7800873e2a5f1535a9e260b294f1e0e14f25d40b
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-18 22:44:24 +0500

    feat: add repository for invite key

---
+++ src/main/java/com/sagansar/todo/repository/InviteKeyRepository.java
@@ -0,0 +1,7 @@
+package com.sagansar.todo.repository;
+
+import com.sagansar.todo.model.work.InviteKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InviteKeyRepository extends JpaRepository<InviteKey, Long> {
+}
