36fafef4f943a30611c04d4fa1608811659a3e34
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-21 03:13:40 +0500

    feat: view for invite screen

---
+++ src/main/webapp/static/js/invite.js
@@ -0,0 +1,56 @@
+let choiceMade = false;
+const token = $("meta[name='_csrf']").attr("content");
+
+function timer(time, domain) {
+    let timer = setInterval(function() {
+        if(time <= 0) {
+            clearInterval(timer);
+            document.location = domain;
+        } else {
+            time--;
+        }
+    }, time)
+}
+
+function sendInviteAnswer(accept) {
+    const xhr = new XMLHttpRequest();
+    const invite = $("#invite-block").attr('value');
+
+    xhr.open("GET", window.location.origin + "/invite/api?id=" + invite + "&accept=" + accept)
+    xhr.setRequestHeader("Content-type", "application/json; charset = utf-8");
+    xhr.setRequestHeader("X-CSRF-TOKEN", token);
+    xhr.send(JSON.stringify(invite));
+    xhr.addEventListener("load", function () {
+        if (xhr.status === 200) {
+            let data = JSON.parse(xhr.response);
+            if (data.error) {
+                console.error("POST " + window.location.origin + "/invite");
+                Swal.fire({
+                    text: data.error,
+                    type: 'error',
+                    confirmButtonColor: '#fb2a79'
+                });
+                choiceMade = false;
+                return;
+            }
+            Swal.fire({
+                type: 'success',
+                text: data.message,
+                //confirmButtonColor: '#195fd4'
+            });
+            timer(50, window.location.origin)
+        }
+    })
+}
+
+$("#accept").click(function () {
+    if (!choiceMade) {
+        sendInviteAnswer(true);
+    }
+});
+
+$("#decline").click(function () {
+    if (!choiceMade) {
+        sendInviteAnswer(false);
+    }
+});
