ae5271569ddc13d9beb3b0fb96e24643236cd7b0
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-21 03:16:29 +0500

    feat: add method for finding

--- src/main/java/com/sagansar/todo/repository/InviteKeyRepository.java
+++ src/main/java/com/sagansar/todo/repository/InviteKeyRepository.java
@@ -4,4 +4,5 @@ import com.sagansar.todo.model.work.InviteKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InviteKeyRepository extends JpaRepository<InviteKey, Long> {
+    InviteKey findDistinctByKey(String key);
 }
