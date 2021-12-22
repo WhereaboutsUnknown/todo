574f449a2c12d6547fd99ec45708ac52bfb4e335
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-22 23:12:39 +0500

    feat: specifications for repositories

--- src/main/java/com/sagansar/todo/repository/TodoTaskRepository.java
+++ src/main/java/com/sagansar/todo/repository/TodoTaskRepository.java
@@ -2,13 +2,14 @@ package com.sagansar.todo.repository;
 
 import com.sagansar.todo.model.work.TodoTask;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TodoTaskRepository extends JpaRepository<TodoTask, Long> {
+public interface TodoTaskRepository extends JpaRepository<TodoTask, Long>, JpaSpecificationExecutor<TodoTask> {
 
     List<TodoTask> findAllByWorkerId(Integer workerId);
 
--- src/main/java/com/sagansar/todo/repository/WorkerRepository.java
+++ src/main/java/com/sagansar/todo/repository/WorkerRepository.java
@@ -2,10 +2,11 @@ package com.sagansar.todo.repository;
 
 import com.sagansar.todo.model.worker.Worker;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WorkerRepository extends JpaRepository<Worker, Integer> {
+public interface WorkerRepository extends JpaRepository<Worker, Integer>, JpaSpecificationExecutor<Worker> {
 
     Worker findByUserId(Integer userId);
 }
