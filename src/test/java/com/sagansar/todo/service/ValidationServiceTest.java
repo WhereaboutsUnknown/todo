729706fde089b394e88b2a65cc522cffae4f8ab9
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-23 21:30:04 +0500

    feat: add unit tests for validation service

---
+++ src/test/java/com/sagansar/todo/service/ValidationServiceTest.java
@@ -0,0 +1,55 @@
+package com.sagansar.todo.service;
+
+import com.sagansar.todo.infrastructure.exceptions.BadRequestException;
+import com.sagansar.todo.telegram.TelegramBotService;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ValidationServiceTest {
+
+    @Autowired
+    ValidationService validationService;
+
+    @Test
+    void test_real_id() {
+        try {
+            validationService.validateVk("id1");
+        } catch (BadRequestException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Test
+    void test_public_id() {
+        assertThrows(BadRequestException.class, () -> validationService.validateVk("public72495085"));
+    }
+
+    @Test
+    void test_404() {
+        assertThrows(BadRequestException.class, () -> validationService.validateVk("adfksfridjfgjddrgjfhghdjhrgsdhfjgd"));
+    }
+
+    @Test
+    void test_deleted() {
+        assertThrows(BadRequestException.class, () -> validationService.validateVk("id9426946"));
+    }
+
+    @Test
+    void test_blocked() {
+        assertThrows(BadRequestException.class, () -> validationService.validateVk("id1132"));
+    }
+
+    @Test
+    void test_not_user() {
+        assertThrows(BadRequestException.class, () -> validationService.validateVk("feed"));
+    }
+}
