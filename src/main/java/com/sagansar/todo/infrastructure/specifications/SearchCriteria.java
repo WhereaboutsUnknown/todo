98b7c04feaa3ac1683f6108a1dda5917e275ac32
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-28 22:40:45 +0500

    fix: boolean resolving in search criteria

--- src/main/java/com/sagansar/todo/infrastructure/specifications/SearchCriteria.java
+++ src/main/java/com/sagansar/todo/infrastructure/specifications/SearchCriteria.java
@@ -1,16 +1,36 @@
 package com.sagansar.todo.infrastructure.specifications;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class SearchCriteria {
     private String key;
     private String operation;
     private Object value;
+
+    public SearchCriteria(String key, String operation, Object value) {
+        this.key = key;
+        this.operation = operation;
+        if ("true".equals(value)) {
+            this.value = Boolean.TRUE;
+        } else if ("false".equals(value)) {
+            this.value = Boolean.FALSE;
+        } else {
+            this.value = value;
+        }
+    }
+
+    public void setValue(Object value) {
+        if ("true".equals(value)) {
+            this.value = Boolean.TRUE;
+        } else if ("false".equals(value)) {
+            this.value = Boolean.FALSE;
+        } else {
+            this.value = value;
+        }
+    }
 }
