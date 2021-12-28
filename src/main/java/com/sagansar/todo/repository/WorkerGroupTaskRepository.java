8599e490add2a72913f4233a3385aff1f468dd1a
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-29 00:15:48 +0500

    feat: add search by task ID

--- src/main/java/com/sagansar/todo/repository/WorkerGroupTaskRepository.java
+++ src/main/java/com/sagansar/todo/repository/WorkerGroupTaskRepository.java
@@ -3,5 +3,8 @@ package com.sagansar.todo.repository;
 import com.sagansar.todo.model.work.WorkerGroupTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkerGroupTaskRepository extends JpaRepository<WorkerGroupTask, Long> {
+    List<WorkerGroupTask> findAllByTaskId(Long taskId);
 }
