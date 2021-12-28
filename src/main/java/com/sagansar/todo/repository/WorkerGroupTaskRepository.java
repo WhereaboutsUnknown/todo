fc2286ce2f93c53388328469c67b2ce21cbd85ca
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-29 00:17:18 +0500

    fix: change method to deletion

--- src/main/java/com/sagansar/todo/repository/WorkerGroupTaskRepository.java
+++ src/main/java/com/sagansar/todo/repository/WorkerGroupTaskRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface WorkerGroupTaskRepository extends JpaRepository<WorkerGroupTask, Long> {
-    List<WorkerGroupTask> findAllByTaskId(Long taskId);
+    void deleteAllByTaskId(Long taskId);
 }
