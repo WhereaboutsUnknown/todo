0758230a8d8fca260b4ad035aa8da0217fd5cfce
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2021-12-31 00:30:21 +0500

    feat: add search by task

--- src/main/java/com/sagansar/todo/repository/WorkerResponseRepository.java
+++ src/main/java/com/sagansar/todo/repository/WorkerResponseRepository.java
@@ -3,5 +3,9 @@ package com.sagansar.todo.repository;
 import com.sagansar.todo.model.worker.WorkerResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkerResponseRepository extends JpaRepository<WorkerResponse, Long> {
+
+    List<WorkerResponse> findAllByTaskIdAndAndCheckedFalse(Long taskId);
 }
