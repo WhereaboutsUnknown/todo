9d92fb6a5807ac1ba7646530021d1f35d9af7119
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-01 23:28:40 +0500

    fix: remove unused import

--- src/main/java/com/sagansar/todo/service/TodoService.java
+++ src/main/java/com/sagansar/todo/service/TodoService.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
