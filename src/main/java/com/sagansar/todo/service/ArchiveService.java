83a61b0129dceaf5bb2a20629885b374683d49a7
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-02 11:37:02 +0500

    feat: add statistics calculation

--- src/main/java/com/sagansar/todo/service/ArchiveService.java
+++ src/main/java/com/sagansar/todo/service/ArchiveService.java
@@ -5,9 +5,8 @@ import com.sagansar.todo.model.external.TaskEstimateTable;
 import com.sagansar.todo.model.manager.Manager;
 import com.sagansar.todo.model.manager.Unit;
 import com.sagansar.todo.model.work.*;
-import com.sagansar.todo.repository.ArchivedTaskRepository;
-import com.sagansar.todo.repository.WorkerArchivedTaskRepository;
-import com.sagansar.todo.repository.WorkerGroupTaskRepository;
+import com.sagansar.todo.model.worker.Worker;
+import com.sagansar.todo.repository.*;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
@@ -21,18 +20,28 @@ import java.util.stream.Collectors;
 @Service
 public class ArchiveService {
 
+    private static final int ARCHIVED = 11;
+
     private final ArchivedTaskRepository archivedTaskRepository;
 
     private final WorkerArchivedTaskRepository workerArchivedTaskRepository;
 
     private final WorkerGroupTaskRepository workerGroupTaskRepository;
 
+    private final StatisticsRepository statisticsRepository;
+
+    private final TodoStatusRepository statusRepository;
+
     public ArchiveService(ArchivedTaskRepository archivedTaskRepository,
                           WorkerArchivedTaskRepository workerArchivedTaskRepository,
-                          WorkerGroupTaskRepository workerGroupTaskRepository) {
+                          WorkerGroupTaskRepository workerGroupTaskRepository,
+                          StatisticsRepository statisticsRepository,
+                          TodoStatusRepository statusRepository) {
         this.archivedTaskRepository = archivedTaskRepository;
         this.workerArchivedTaskRepository = workerArchivedTaskRepository;
         this.workerGroupTaskRepository = workerGroupTaskRepository;
+        this.statisticsRepository = statisticsRepository;
+        this.statusRepository = statusRepository;
     }
 
     /**
@@ -54,14 +63,18 @@ public class ArchiveService {
         archivedTask.setCreator(creator);
         Map<Integer, Integer> estimates = estimateTables.stream()
                 .collect(Collectors.toMap(TaskEstimateTable::getWorkerId, TaskEstimateTable::getEstimate));
-        workerArchivedTaskRepository.saveAll(
-                workerGroupTaskRepository.findAllByTaskId(task.getId()).stream()
-                        .filter(Objects::nonNull)
-                        .map(workerGroupTask -> createRelation(workerGroupTask, estimates.get(workerGroupTask.getWorker().getId())))
-                        .collect(Collectors.toList())
-        );
+        List<WorkerArchivedTask> workers = workerGroupTaskRepository.findAllByTaskId(task.getId()).stream()
+                .filter(Objects::nonNull)
+                .map(workerGroupTask -> createRelation(workerGroupTask, estimates.get(workerGroupTask.getWorker().getId())))
+                .collect(Collectors.toList());
+        workerArchivedTaskRepository.saveAll(workers);
         archivedTask.setArchiveTime(LocalDateTime.now(ZoneId.systemDefault()));
         archivedTaskRepository.save(archivedTask);
+        calculateStatistics(workers.stream()
+                .map(WorkerArchivedTask::getWorker)
+                .collect(Collectors.toList()));
+
+        task.setStatus(archived());
         return task;
     }
 
@@ -78,6 +91,74 @@ public class ArchiveService {
         }
         return archivedTaskRepository.findById(id)
                 .orElseThrow(() -> new BadRequestException("Архивная задача не найдена!"));
+    }
+
+    public Statistics getWorkerStatistics(@NonNull Integer workerId) {
+        return statisticsRepository.findDistinctByWorkerId(workerId);
+    }
+
+    public List<Statistics> getStatistics(@NonNull List<Integer> ids) {
+        return statisticsRepository.findAllByWorkerIdIn(ids);
+    }
+
+    /**
+     * Increase counter of rejected tasks
+     *
+     * @param worker Worker
+     */
+    public void increaseRejected(@NonNull Worker worker) {
+        Statistics statistics = getWorkerStatistics(worker.getId());
+        if (statistics == null) {
+            statistics = new Statistics();
+            statistics.setWorker(worker);
+        }
+        int rejected = statistics.getRejected() == null ? 0 : statistics.getRejected();
+        statistics.setRejected(rejected + 1);
+        statisticsRepository.save(statistics);
+    }
+
+    /**
+     * Recalculate statistics for Workers
+     *
+     * @param workers Workers list
+     */
+    public void calculateStatistics(@NonNull List<Worker> workers) {
+        statisticsRepository.saveAll(workers.stream()
+                .map(this::calculateStatistics)
+                .collect(Collectors.toList()));
+    }
+
+    /**
+     * Recalculate statistics for Worker
+     *
+     * @param worker Worker
+     * @return calculated statistics
+     */
+    private Statistics calculateStatistics(@NonNull Worker worker) {
+        int tasksCount = archivedTaskRepository.countAllByWorkerId(worker.getId());
+        int successCount = archivedTaskRepository.countAllByWorkerIdAndSuccessfulTrue(worker.getId());
+        int failedCount = tasksCount - successCount;
+
+        List<WorkerArchivedTask> tasks = workerArchivedTaskRepository.findAllByWorkerId(worker.getId());
+        int responsible = 0;
+        double pointsTotal = 0.0;
+        for (WorkerArchivedTask task : tasks) {
+            if (task.isResponsible()) {
+                responsible++;
+            }
+            pointsTotal += task.getEstimate();
+        }
+        Statistics statistics = getWorkerStatistics(worker.getId());
+        if (statistics == null) {
+            statistics = new Statistics();
+            statistics.setWorker(worker);
+        }
+        statistics.setCalculatedAt(LocalDateTime.now(ZoneId.systemDefault()));
+        statistics.setDone(successCount);
+        statistics.setFailed(failedCount);
+        statistics.setResponsible(responsible);
+        statistics.setPoints(pointsTotal / tasks.size());
+        return statistics;
     }
 
     private ArchivedTask createArchivedTask(@NonNull TodoTask task, @NonNull Manager manager) {
@@ -99,5 +180,9 @@ public class ArchiveService {
         workerArchivedTask.setResponsible(workerGroupTask.isResponsible());
         workerArchivedTask.setEstimate(estimate);
         return workerArchivedTask;
+    }
+
+    private TodoStatus archived() {
+        return statusRepository.findById(ARCHIVED).orElseThrow(() -> new RuntimeException("Статус архивных задач не найден"));
     }
 }
