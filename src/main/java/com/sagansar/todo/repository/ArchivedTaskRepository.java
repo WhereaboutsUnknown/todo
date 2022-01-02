f3a826a13ccd93f25c8c89dc3be1e8292890eb2a
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-02 11:36:25 +0500

    feat: add repo methods for statistics

--- src/main/java/com/sagansar/todo/repository/ArchivedTaskRepository.java
+++ src/main/java/com/sagansar/todo/repository/ArchivedTaskRepository.java
@@ -4,4 +4,8 @@ import com.sagansar.todo.model.work.ArchivedTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArchivedTaskRepository extends JpaRepository<ArchivedTask, Long> {
+
+    int countAllByWorkerId(Integer workerId);
+
+    int countAllByWorkerIdAndSuccessfulTrue(Integer workerId);
 }
--- src/main/java/com/sagansar/todo/repository/WorkerArchivedTaskRepository.java
+++ src/main/java/com/sagansar/todo/repository/WorkerArchivedTaskRepository.java
@@ -3,5 +3,9 @@ package com.sagansar.todo.repository;
 import com.sagansar.todo.model.work.WorkerArchivedTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkerArchivedTaskRepository extends JpaRepository<WorkerArchivedTask, Long> {
+
+    List<WorkerArchivedTask> findAllByWorkerId(Integer workerId);
 }
