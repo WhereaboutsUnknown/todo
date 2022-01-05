95d09c75d0be2a39908b24995f59aea9af67c88e
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-06 01:38:07 +0500

    feat: add registration form

---
+++ src/main/java/com/sagansar/todo/model/external/RegistrationForm.java
@@ -0,0 +1,32 @@
+package com.sagansar.todo.model.external;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RegistrationForm {
+
+    @JsonProperty
+    protected String username;
+
+    @JsonProperty
+    protected String password;
+
+    @JsonProperty
+    protected String firstName;
+
+    @JsonProperty
+    protected String patronym;
+
+    @JsonProperty
+    protected String surname;
+
+    @JsonProperty
+    private LocalDate birthDate;
+}
