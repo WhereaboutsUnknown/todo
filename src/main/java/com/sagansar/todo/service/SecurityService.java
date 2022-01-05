01bbb8b26c8f899d82a775c4248b0009a944800b
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-05 17:46:04 +0500

    feat: add registration with password generation

--- src/main/java/com/sagansar/todo/service/SecurityService.java
+++ src/main/java/com/sagansar/todo/service/SecurityService.java
@@ -1,17 +1,21 @@
 package com.sagansar.todo.service;
 
+import com.sagansar.todo.infrastructure.exceptions.BadRequestException;
 import com.sagansar.todo.infrastructure.exceptions.UnauthorizedException;
+import com.sagansar.todo.infrastructure.validation.Validator;
+import com.sagansar.todo.model.general.Contacts;
+import com.sagansar.todo.model.general.Role;
 import com.sagansar.todo.model.general.RoleEnum;
 import com.sagansar.todo.model.general.User;
 import com.sagansar.todo.model.manager.Manager;
 import com.sagansar.todo.model.work.Invite;
 import com.sagansar.todo.model.work.InviteKey;
 import com.sagansar.todo.model.worker.Worker;
-import com.sagansar.todo.repository.InviteKeyRepository;
-import com.sagansar.todo.repository.ManagerRepository;
-import com.sagansar.todo.repository.UserRepository;
-import com.sagansar.todo.repository.WorkerRepository;
+import com.sagansar.todo.repository.*;
 import lombok.AllArgsConstructor;
+import org.passay.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
@@ -19,6 +23,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
@@ -26,16 +31,24 @@ import java.time.ZoneId;
 import java.util.Objects;
 import java.util.Set;
 
+import static org.passay.CharacterOccurrencesRule.ERROR_CODE;
+
 @Service
 @AllArgsConstructor
 public class SecurityService {
 
+    private static final Logger logger = LoggerFactory.getLogger(SecurityService.class);
+
     private final UserDetailsServiceImpl userDetailsService;
     private final UserRepository userRepository;
+    private final ContactsRepository contactsRepository;
+    private final RoleRepository roleRepository;
     private final ManagerRepository managerRepository;
     private final WorkerRepository workerRepository;
     private final InviteKeyRepository inviteKeyRepository;
     private final PasswordEncoder encoder;
+    private final PasswordGenerator passwordGenerator;
+    private final Validator passwordValidator;
 
     public User getCurrentUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -116,5 +129,100 @@ public class SecurityService {
         }
         inviteKeyRepository.delete(key);
         return invite;
+    }
+
+    public String generatePassword() {
+        return passwordGenerator.generatePassword(10, generationRules());
+    }
+
+    public boolean registerWorkerFromTelegram(String username, String password) {
+        try {
+            User user = registerUser(createUser(username, password, RoleEnum.FREELANCER));
+            Contacts contacts = new Contacts();
+            contacts.setTelegram(username);
+            contacts.setUser(user);
+            contactsRepository.save(contacts);
+            registerWorker(user);
+            return true;
+        } catch (BadRequestException e) {
+            logger.error(e.getResponseMessage());
+            return false;
+        }
+    }
+
+    public Worker registerWorker(@NonNull User user) {
+        Worker worker = createWorker();
+        worker.setUser(user);
+        worker.setActive(true);
+        return workerRepository.save(worker);
+    }
+
+    public Manager registerManager(@NonNull User user) {
+        Manager manager = createManager();
+        manager.setUser(user);
+        manager.setActive(true);
+        return managerRepository.save(manager);
+    }
+
+    private User registerUser(@NonNull User user) throws BadRequestException {
+        user.setActive(true);
+        return userRepository.save(user);
+    }
+
+    private User createUser(String username, String password, RoleEnum role) throws BadRequestException {
+        if (userRepository.existsByUsername(username)) {
+            throw new BadRequestException("Пользователь с таким именем уже существует");
+        }
+        passwordValidator.validate(password);
+        User user = new User();
+        user.setUsername(username);
+        String encodedPassword = encoder.encode(password);
+        user.setPassword(encodedPassword);
+        Role userRole = roleRepository.findByName(role.name());
+        if (userRole == null) {
+            throw new BadRequestException("Отсутствует роль пользователя");
+        }
+        user.setRoles(Set.of(userRole));
+        if (RoleEnum.SUPERVISOR.equals(role)) {
+            Role manager = roleRepository.findByName(RoleEnum.MANAGER.name());
+            user.getRoles().add(manager);
+        }
+        return user;
+    }
+
+    private Worker createWorker() {
+        return new Worker();
+    }
+
+    private Manager createManager() {
+        return new Manager();
+    }
+
+    private CharacterRule[] generationRules() {
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(2);
+
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(2);
+
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        digitRule.setNumberOfCharacters(2);
+
+        CharacterData specialChars = new CharacterData() {
+            public String getErrorCode() {
+                return ERROR_CODE;
+            }
+
+            public String getCharacters() {
+                return "!@#$%^&*()_+";
+            }
+        };
+        CharacterRule splCharRule = new CharacterRule(specialChars);
+        splCharRule.setNumberOfCharacters(2);
+
+        return new CharacterRule[] { lowerCaseRule, upperCaseRule, digitRule, splCharRule };
     }
 }
