6e66958619d3191d54b4921ccd91412ab5772721
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-06 15:07:48 +0500

    feat: add multipart resolver

--- src/main/java/com/sagansar/todo/WebConfiguration.java
+++ src/main/java/com/sagansar/todo/WebConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerF
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
@@ -43,5 +44,12 @@ public class WebConfiguration implements WebMvcConfigurer {
             container.addErrorPages(new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR,
                     "/error"));
         };
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(1024 * 1024 * 100);
+        return multipartResolver;
     }
 }
