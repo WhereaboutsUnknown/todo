e01627046f47cabcc8064d9bd76278d372846236
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-08 23:15:56 +0500

    refactor: script refactoring

--- src/main/webapp/static/js/invite.js
+++ src/main/webapp/static/js/invite.js
@@ -1,38 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
     let choiceMade = false;
-    const token = $("meta[name='_csrf']").attr("content");
-
-    function timer(time, domain) {
-        let timer = setInterval(function() {
-            if(time <= 0) {
-                clearInterval(timer);
-                document.location = domain;
-            } else {
-                time--;
-            }
-        }, time)
-    }
 
     function sendInviteAnswer(accept) {
         const invite = $("#invite-block").attr('value');
 
         rest("POST", "/invite/api?id=" + invite + "&accept=" + accept, null, function (data) {
             if (data.error) {
-                console.error("POST " + window.location.origin + "/invite");
-                Swal.fire({
-                    text: data.error,
-                    type: 'error',
-                    confirmButtonColor: '#fb2a79'
-                });
+                console.error("POST " + api() + "/invite", "403", data.error);
+                showError(data.error);
                 choiceMade = false;
                 return;
             }
-            Swal.fire({
-                type: 'success',
-                text: data.message,
-                //confirmButtonColor: '#195fd4'
-            });
-            timer(50, window.location.origin)
+            showDone(data.message);
+            redirectTimer(3000, root());
         });
     }
 
