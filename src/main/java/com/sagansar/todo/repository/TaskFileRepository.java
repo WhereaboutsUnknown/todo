fe4dca22641584d8da064868aa11c39223b7a1af
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-08 23:31:17 +0500

    refactor: rename repo

--- src/main/java/com/sagansar/todo/repository/TaskFileRepository.java
+++ src/main/java/com/sagansar/todo/repository/TaskFileRepository.java
@@ -3,5 +3,5 @@ package com.sagansar.todo.repository;
 import com.sagansar.todo.model.work.TaskFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<TaskFile, Long> {
+public interface TaskFileRepository extends JpaRepository<TaskFile, Long> {
 }
