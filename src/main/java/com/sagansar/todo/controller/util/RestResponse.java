7fa5afbb449ac12461d65b5fdfe05da1e549318f
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-08 23:34:58 +0500

    feat: add rest response messages

---
+++ src/main/java/com/sagansar/todo/controller/util/RestResponse.java
@@ -0,0 +1,12 @@
+package com.sagansar.todo.controller.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestResponse {
+    private String message;
+}
