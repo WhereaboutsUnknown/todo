d53c277914484f4ec676d8bb0d38887ac89faef5
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-12 09:08:45 +0500

    feat: file purpose

---
+++ src/main/java/com/sagansar/todo/model/general/FilePurpose.java
@@ -0,0 +1,29 @@
+package com.sagansar.todo.model.general;
+
+import lombok.Getter;
+
+@Getter
+public enum FilePurpose {
+    USER_AVATAR(1L, Extension.Type.IMAGE, "user.jpg"),
+    TASK_VIDEO(2L, Extension.Type.VIDEO, ""),
+    UNKNOWN(0, Extension.Type.FILE, "");
+
+    private final Long id;
+    private final Extension.Type type;
+    private final String defaultFile;
+
+    FilePurpose(long id, Extension.Type type, String defaultFile) {
+        this.id = id;
+        this.type = type;
+        this.defaultFile = defaultFile;
+    }
+
+    public static FilePurpose fromId(long id) {
+        for (FilePurpose purpose : values()) {
+            if (purpose.id == id) {
+                return purpose;
+            }
+        }
+        return UNKNOWN;
+    }
+}
