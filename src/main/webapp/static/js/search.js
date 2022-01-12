7d5d273fceaeb5771f3f044edd5de4845fb63f75
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-12 09:00:49 +0500

    feat: add search url building

--- src/main/webapp/static/js/search.js
+++ src/main/webapp/static/js/search.js
@@ -9,6 +9,38 @@ function getSort() {
     return 'desc';
 }
 
+function getSortSelector() {
+    return $('#sort-select-input').val();
+}
+
+function getWorkerCriteria() {
+
+}
+
+function getTaskCriteria() {
+
+}
+
+function getCriteria(type) {
+    if (type) {
+        if (type === 'task') {
+
+        }
+        else if (type === 'worker') {
+
+        }
+    }
+    return null;
+}
+
+function addSearchParams(url, page, size, type) {
+    const sortDir = getSort();
+    const sortBy = getSortSelector();
+    const criteria = getCriteria(type);
+
+    return url + "?" + "dir=" + sortDir + (sortBy ? "&by=" + sortBy : "") + (criteria ? "&crit=" + criteria : "") + "&page=" + (page ? page : 0) + "&size=" + (size ? size : 50);
+}
+
 sortBtn().click(function () {
     if ($(this).hasClass('direction-up')) {
         $(this).removeClass('direction-up');
