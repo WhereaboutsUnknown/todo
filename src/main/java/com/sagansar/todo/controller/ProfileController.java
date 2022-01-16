7fb1d47e59dc683b707af611c8e58f6a4834a372
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-16 23:59:00 +0500

    feat: add current user profile GET

--- src/main/java/com/sagansar/todo/controller/ProfileController.java
+++ src/main/java/com/sagansar/todo/controller/ProfileController.java
@@ -1,7 +1,16 @@
 package com.sagansar.todo.controller;
 
+import com.sagansar.todo.controller.dto.PersonNameDto;
+import com.sagansar.todo.controller.mapper.PersonMapper;
 import com.sagansar.todo.controller.util.RestResponse;
 import com.sagansar.todo.infrastructure.exceptions.BadRequestException;
+import com.sagansar.todo.infrastructure.exceptions.UserBlockedException;
+import com.sagansar.todo.model.general.RoleEnum;
+import com.sagansar.todo.model.general.User;
+import com.sagansar.todo.model.manager.Manager;
+import com.sagansar.todo.model.worker.Worker;
+import com.sagansar.todo.repository.ManagerRepository;
+import com.sagansar.todo.repository.WorkerRepository;
 import com.sagansar.todo.service.SecurityService;
 import com.sagansar.todo.service.ValidationService;
 import lombok.AllArgsConstructor;
@@ -10,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin
 @Transactional
 @AllArgsConstructor
 @ResponseBody
@@ -19,6 +29,10 @@ public class ProfileController {
     private final SecurityService securityService;
 
     private final ValidationService validationService;
+
+    private final ManagerRepository managerRepository;
+
+    private final WorkerRepository workerRepository;
 
     @PostMapping("/restore")
     public RestResponse restoreProfile() throws BadRequestException {
@@ -38,5 +52,29 @@ public class ProfileController {
         validationService.checkNullSafety(id);
         securityService.blockWorkerProfile(id, true);
         return new RestResponse("Профиль удален!");
+    }
+
+    @GetMapping
+    public PersonNameDto getCurrentUserProfile() {
+        User user = securityService.getCurrentUser();
+        if (user != null && user.isActive()) {
+            if (securityService.checkUserRights(RoleEnum.MANAGER)) {
+                Manager manager = managerRepository.findByUserId(user.getId())
+                        .orElseThrow(() -> new UserBlockedException("Не найден профиль менеджера!"));
+                if (!manager.isActive()) {
+                    throw new UserBlockedException("Профиль менеджера не активен!");
+                }
+                return PersonMapper.managerToName(manager);
+            }
+            if (securityService.checkUserRights(RoleEnum.FREELANCER)) {
+                Worker worker = workerRepository.findById(user.getId())
+                        .orElseThrow(() -> new UserBlockedException("Не найден профиль исполнителя!"));
+                if (!worker.isActive()) {
+                    throw new UserBlockedException("Профиль исполнителя не активен!");
+                }
+                return PersonMapper.workerToName(worker);
+            }
+        }
+        throw new UserBlockedException(user == null ? "Пользователь не найден!" : "Пользователь заблокирован!");
     }
 }
