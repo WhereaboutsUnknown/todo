270a2e3149fbf01cf3d37faff0b63cb1524a392a
WhereaboutsUnknown <vyacheslav.stepanov.1994@mail.ru>
2022-01-23 21:54:16 +0500

    feat: add getting task from invite

--- src/main/java/com/sagansar/todo/controller/InviteRestController.java
+++ src/main/java/com/sagansar/todo/controller/InviteRestController.java
@@ -1,11 +1,14 @@
 package com.sagansar.todo.controller;
 
 import com.sagansar.todo.controller.dto.InviteDto;
+import com.sagansar.todo.controller.dto.TaskBasic;
 import com.sagansar.todo.controller.mapper.InviteMapper;
+import com.sagansar.todo.controller.mapper.TaskMapper;
 import com.sagansar.todo.controller.util.RestResponse;
 import com.sagansar.todo.infrastructure.exceptions.BadRequestException;
 import com.sagansar.todo.model.work.Invite;
 import com.sagansar.todo.model.worker.Worker;
+import com.sagansar.todo.repository.InviteRepository;
 import com.sagansar.todo.service.ArchiveService;
 import com.sagansar.todo.service.InviteService;
 import com.sagansar.todo.service.SecurityService;
@@ -34,6 +37,8 @@ public class InviteRestController {
 
     private final SecurityService securityService;
 
+    private final InviteRepository inviteRepository;
+
     @GetMapping ("")
     public RestResponse processInviteAnswer(@RequestParam(name = "id") Long inviteId, @RequestParam(name = "accept") boolean accept) throws BadRequestException {
         Invite invite = inviteService.processInviteAnswer(inviteId, accept);
@@ -54,5 +59,16 @@ public class InviteRestController {
         return invites.stream()
                 .map(InviteMapper::inviteToDto)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/worker/{workerId}/{id}")
+    public TaskBasic getTaskFromInvite(@PathVariable Integer workerId, @PathVariable Long id) throws BadRequestException {
+        securityService.getAuthorizedWorker(workerId);
+        Invite invite = inviteRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Приглашение не найдено!"));
+        if (!workerId.equals(invite.getWorker().getId())) {
+            throw new BadRequestException("Приглашение адресовано другому исполнителю!");
+        }
+        return TaskMapper.taskToBasic(invite.getTask());
     }
 }
