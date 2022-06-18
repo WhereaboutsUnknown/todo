dc99e87599b12678273fd4fa47ca64d57dd60f29
WhereaboutsUnknown <54309961+WhereaboutsUnknown@users.noreply.github.com>
2022-06-18 21:21:30 +0500

    feat: add service and controller for kanban board editing

---
+++ src/main/java/com/sagansar/todo/kanban/KanbanManageController.java
@@ -0,0 +1,92 @@
+package com.sagansar.todo.kanban;
+
+import com.sagansar.todo.controller.dto.PersonNameDto;
+import com.sagansar.todo.controller.mapper.Mapper;
+import com.sagansar.todo.kanban.dto.KanbanColumnEdit;
+import com.sagansar.todo.kanban.dto.KanbanColumnRead;
+import com.sagansar.todo.kanban.dto.KanbanColumnShort;
+import com.sagansar.todo.kanban.dto.KanbanTicketFetch;
+import com.sagansar.todo.kanban.model.KanbanColumn;
+import com.sagansar.todo.kanban.service.KanbanManageService;
+import com.sagansar.todo.service.SecurityService;
+import com.sagansar.todo.service.TodoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@Transactional
+@AllArgsConstructor
+@ResponseBody
+@RequestMapping(path = "/tasks/{taskId}/kanban/manage", produces = MediaType.APPLICATION_JSON_VALUE)
+public class KanbanManageController {
+
+    private final KanbanManageService kanbanManageService;
+    private final SecurityService securityService;
+    private final TodoService todoService;
+
+    @PostMapping("/col")
+    public KanbanColumnRead create(
+            @PathVariable Long taskId,
+            @RequestBody KanbanColumnEdit view
+    ) throws Exception {
+        checkAccess(taskId);
+
+        var board = kanbanManageService.getBoard(taskId);
+        var column = new KanbanColumn();
+        Mapper.map(view, column);
+        column.setBoard(board);
+        column.setOrder(kanbanManageService.getNextOrder(board));
+        column = kanbanManageService.save(column);
+        var response = new KanbanColumnRead();
+        Mapper.map(column, response);
+
+        return response;
+    }
+
+    @PutMapping("/col/{id}")
+    public KanbanColumnShort update(
+            @PathVariable Long taskId,
+            @PathVariable Long id,
+            @RequestBody KanbanColumnEdit view
+    ) throws Exception {
+        checkAccess(taskId);
+
+        var column = kanbanManageService.get(id, taskId);
+        Mapper.map(view, column);
+        column = kanbanManageService.save(column);
+        var response = new KanbanColumnShort();
+        Mapper.map(column, response);
+
+        return response;
+    }
+
+    @DeleteMapping("/col/{id}")
+    public List<KanbanTicketFetch> delete(
+            @PathVariable Long taskId,
+            @PathVariable Long id
+    ) throws Exception {
+        checkAccess(taskId);
+
+        var column = kanbanManageService.get(id, taskId);
+        var backlogContent = kanbanManageService.delete(column);
+        return backlogContent.stream()
+                .map(t -> {
+                    var response = new KanbanTicketFetch();
+                    Mapper.map(t, response);
+                    var worker = new PersonNameDto();
+                    Mapper.map(t.getWorker(), worker);
+                    response.setWorker(worker);
+                    return response;
+                })
+                .collect(Collectors.toList());
+    }
+
+    private void checkAccess(Long taskId) throws Exception {
+        todoService.checkUserRightsOnTaskAsWorker(securityService.getCurrentUser().getId(), taskId);
+    }
+}
---
+++ src/main/java/com/sagansar/todo/kanban/service/KanbanManageService.java
@@ -0,0 +1,85 @@
+package com.sagansar.todo.kanban.service;
+
+import com.sagansar.todo.infrastructure.exceptions.BadRequestException;
+import com.sagansar.todo.kanban.model.Kanban;
+import com.sagansar.todo.kanban.model.KanbanColumn;
+import com.sagansar.todo.kanban.model.KanbanTicket;
+import com.sagansar.todo.kanban.repository.KanbanColumnRepository;
+import com.sagansar.todo.kanban.repository.KanbanRepository;
+import com.sagansar.todo.kanban.repository.KanbanTicketRepository;
+import com.sagansar.todo.model.work.TodoTask;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class KanbanManageService {
+
+    private final KanbanColumnRepository kanbanColumnRepository;
+    private final KanbanRepository kanbanRepository;
+    private final KanbanTicketRepository kanbanTicketRepository;
+
+    public KanbanColumn get(Long id, Long taskId) throws Exception {
+        return kanbanColumnRepository.findByBoardTaskIdAndId(taskId, id)
+                .orElseThrow(() -> new BadRequestException("Столбец не найден"));
+    }
+
+    public Integer getNextOrder(Kanban kanban) {
+        var maxOrder = kanbanColumnRepository.maxOrder(kanban.getId());
+        return Objects.requireNonNullElse(maxOrder, 0) + 1;
+    }
+
+    public Kanban getBoard(Long taskId) throws Exception {
+        return kanbanRepository.findByTaskId(taskId)
+                .orElseThrow(() -> new BadRequestException("Доска не найдена"));
+    }
+
+    public void attachBoard(TodoTask task) {
+        var kanban = kanbanRepository.findByTaskId(task.getId()).orElse(new Kanban());
+        kanban.setTask(task);
+        kanbanRepository.save(kanban);
+
+        var backlog = new KanbanColumn();
+        backlog.setDeletable(false);
+        backlog.setFinishing(false);
+        backlog.setOrder(0);
+        backlog.setName("Бэклог");
+        backlog.setBoard(kanban);
+        kanbanColumnRepository.save(backlog);
+
+        var done = new KanbanColumn();
+        done.setDeletable(false);
+        done.setFinishing(true);
+        done.setOrder(-1);
+        done.setName("Сделано!");
+        done.setBoard(kanban);
+        kanbanColumnRepository.save(done);
+    }
+
+    public KanbanColumn save(KanbanColumn column) {
+        return kanbanColumnRepository.save(column);
+    }
+
+    public List<KanbanTicket> delete(KanbanColumn column) throws Exception {
+        if (!column.getDeletable()) {
+            throw new BadRequestException("Столбец нельзя удалить");
+        }
+        var backlogContent = moveAllToBacklog(column);
+        kanbanColumnRepository.delete(column);
+
+        return backlogContent;
+    }
+
+    private List<KanbanTicket> moveAllToBacklog(KanbanColumn column) {
+        var backlog = kanbanColumnRepository
+                .findByBoardTaskIdAndDeletableFalseAndFinishingFalse(column.getBoard().getTask().getId())
+                .orElseThrow(() -> new RuntimeException("Бэклог не найден"));
+        var tickets = kanbanTicketRepository.findAllByColumnId(column.getId());
+        tickets.forEach(t -> t.setColumn(backlog));
+        kanbanTicketRepository.saveAll(tickets);
+        return kanbanTicketRepository.findAllByColumnId(backlog.getId());
+    }
+}
